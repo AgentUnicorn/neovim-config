ea08edf3b6e2df7d523402aac0588ca85583398a
Shiro <superslap1999@gmail.com>
2025-03-26 16:53:00 +0700

    Add conform

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -5,3 +5,6 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 vim.keymap.set('n', 'gl', function () vim.diagnostic.open_float() end, { desc = 'Open diagnostic in float' })
 
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Find and replace" })
+
+vim.keymap.set('n', '<C-a>', 'gg<S-v>G', { desc = 'Select all' })
