982d922cc438b083739c2942f6e9e54799ce8063
shiro <superslap1999@gmail.com>
2025-04-03 19:38:16 +0700

    Add macros

--- init.lua
+++ init.lua
@@ -1,1 +1,16 @@
 require("config.lazy")
+
+-- Custom Macros
+local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
+vim.api.nvim_create_augroup("JSLogMacro", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+	group = "JSLogMacro",
+	pattern = {
+		"javacript",
+		"typescript",
+	},
+	callback = function()
+		vim.fn.setreg("l", "yoconsole.log('" .. esc .. "pa:" .. esc .. "la, " .. esc .. "pl")
+	end,
+})
